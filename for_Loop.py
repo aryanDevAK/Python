a4c4b5807759dfb6b821ac20e87d6dca49f720c7
AK-Developers <aryankhatri9811@gmail.com>
2024-01-12 21:49:51 +0530

    practice

---
+++ for_Loop.py
@@ -0,0 +1,21 @@
+#For Loop : A statement that will execute its block of code a limited number of times.
+
+# for i in range(10):
+    # print(i)
+
+# for i in range(1,10):
+    # print(i)
+
+# for i in range(2,100,2):
+    # print(i)
+
+# name ="Aryan Khatri"
+# for i in name:
+    # print(i.capitalize(),end="")
+
+import time
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+
+print("Happy New Year!")
