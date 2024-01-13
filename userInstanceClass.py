8b270f444b12f0a5220193f36d99a54f66140c2d
AK-Developers <aryankhatri9811@gmail.com>
2024-01-13 18:37:45 +0530

    practice

---
+++ userInstanceClass.py
@@ -0,0 +1,17 @@
+class Member:
+    def __init__(self,username,fullName):
+        self.username=username
+        self.fullName=fullName
+
+guy1 = Member("Gugu","Ruhi Sharma")
+guy2 = Member("Kaku","Aryan Khatri")
+
+print(guy1)
+print(guy1.fullName)
+print(guy1.username)
+print(type(guy1))
+
+print(guy2)
+print(guy2.fullName)
+print(guy2.username)
+print(type(guy2))
